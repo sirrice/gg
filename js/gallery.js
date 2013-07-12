bea306703786494b7d952e81344fcc92d5fc3e85
eugene wu <sirrice@gmail.com>
2013-07-12 14:31:29 -0400

    added gallery example

---
+++ js/gallery.js
@@ -0,0 +1,61 @@
+var all_specs = [
+  geom_area
+  ,geom_boxplot
+  ,geom_interval
+  ,geom_point_1
+  ,geom_point_sum
+  ,geom_point_interval
+  ,geom_point_2
+  ,geom_point_3
+  ,geom_point_4
+  ,colored_lines
+  ,colored_lines_multi
+  ,geom_boxplot2
+];
+
+
+
+;(function (all_specs) {
+
+  var render = function(specs) {
+    if(!("layers" in specs)) {
+      specs = _.flatten([specs]);
+      specs = {layers: specs};
+    }
+    var ex = function () {
+      return d3.select("#gallery").append("span");
+    };
+    specs.data = genData(50);
+    if (!specs.opts)  specs.opts = {};
+    specs.opts.w = 400;
+    specs.opts.h = 300;
+    
+    var plot = gg(specs);
+    plot.render(ex());
+  }
+
+  var genData = function(npts) {
+    Math.seedrandom("zero");
+    var gauss = science.stats.distribution.gaussian();
+
+    bigdata = _.map(_.range(0, npts), function(d) {
+      g = Math.floor(Math.random() * 3) + 1;
+      f = Math.floor(Math.random() * 3);
+      t = Math.floor(Math.random() * 2);
+      gauss.variance(d * 30.0 / npts);
+
+      return {d: Math.floor(d/3), e: ((d + gauss())*(2+Math.sin(d/50))) * (g) - (d),  g: g, f:f, t:t};
+    });
+
+    return bigdata;
+  }
+
+
+  $(document).ready(function() {
+     _.each(all_specs, render);
+
+  });
+})(all_specs);
+
+
+
